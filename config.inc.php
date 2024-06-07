44cf3fa00b1ad44d64c6e195133e892b530cce68
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2024-06-07 10:02:05 +0800

    add file

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,14 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户名',
-  'password' => '数据库密码',
+  'host' => '74.48.17.20',
+  'port' => 3306,
+  'user' => 'typecho',
+  'password' => 'typecho123',
   'charset' => 'utf8mb4',
-  'port' => '3306',
-  'database' => '数据库名称',
-  'engine' => 'MyISAM',
+  'database' => 'typecho',
+  'engine' => 'InnoDB',
+  'sslCa' => '',
+  'sslVerify' => true,
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
